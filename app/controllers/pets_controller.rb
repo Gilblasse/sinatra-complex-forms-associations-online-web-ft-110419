b26ad504366e08a5eadfa3b684414ad6a0fb27c8
Nethelbert <gilblasse@gmail.com>
2019-12-23 20:01:56 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,16 +6,24 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-    
+    @pet = Pet.create(name: params[:pet][:name]) if !params[:pet][:name].empty?
+    if !params[:pet][:owner_ids].nil?
+      @pet.owner = Owner.find(params[:pet][:owner_ids]).first
+      @pet.save
+    end
+    # @pet.create_owner(name: params[:pet][:owner]) if !params[:pet][:owner].empty?
+    if !params[:pet][:owner].empty?
     redirect to "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
+    
     erb :'/pets/show'
   end
 
